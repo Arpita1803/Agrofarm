d68b4ce4302b8e1e11e23df22325b7a7ec130464
Arpita1803 <arpitahule1810@gmail.com>
2026-02-12 12:16:16 +0530

    cfrwdvfsv
    hyrh

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -25,6 +25,33 @@ const authMiddleware = (req, res, next) => {
 export default authMiddleware;
 
 
+// import jwt from "jsonwebtoken";
+
+// const authMiddleware = (req, res, next) => {
+//   try {
+//     const authHeader = req.headers.authorization;
+
+//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//       return res.status(401).json({ message: "No token, authorization denied" });
+//     }
+
+//     const token = authHeader.split(" ")[1];
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     req.user = {
+//       id: decoded.id,
+//       role: decoded.role,
+//     };
+
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: "Token is not valid" });
+//   }
+// };
+
+// export default authMiddleware;
+
+
 
 
    // import bcrypt from "bcryptjs";
